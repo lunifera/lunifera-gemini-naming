3b210c2aa3b040833c63ed6cd257370a8876f1e9
Violeta Georgieva <milesg78@gmail.com>
2012-05-03 16:48:34 +0000

    bug 377313: URL Context have to be used instead of Default Context when invoking methods from org.eclipse.gemini.naming.ContextWrapperImpl in case a URL string is passed as a name parameter.

--- framework/src/main/java/org/eclipse/gemini/naming/ContextWrapperImpl.java
+++ framework/src/main/java/org/eclipse/gemini/naming/ContextWrapperImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Oracle.
+ * Copyright (c) 2010, 2012 Oracle.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Apache License v2.0 which accompanies this distribution. 
@@ -42,153 +42,100 @@ class ContextWrapperImpl implements Context {
 
 
 	public Object lookup(Name name) throws NamingException {
-		return m_context.lookup(name);
+		return getURLContextOrDefaultContext(name.toString()).lookup(name);
 	}
 
 	public Object lookup(String name) throws NamingException {
-		if (isURLRequest(name)) {
-			// attempt to find a URL Context Factory to satisfy this lookup
-			// request
-			ObjectFactory objectFactory = null;
-			try {
-				// obtain URL Context Factory in a doPrivilieged() block
-				objectFactory = (ObjectFactory)SecurityUtils.invokePrivilegedAction(new GetObjectFactoryAction(m_factoryManager, name));
-			} catch (Exception e) {
-				logger.log(Level.FINE, 
-						   "Exception occurred while trying to obtain a reference to a URL Context Factory.",
-						   e);
-			}
-			
-			if (objectFactory == null) {
-				throw new NameNotFoundException(
-						"Name: "
-								+ name
-								+ " was not found.  A URL Context Factory was not registered to handle "
-								+ "this URL scheme");
-			}
-
-			try {
-				Context context = 
-					(Context) objectFactory.getObjectInstance(null, null, 
-															  null, m_context.getEnvironment());
-				if (context != null) {
-					return context.lookup(name);
-				}
-				else {
-					throw new NamingException("Name = " + name
-							+ "was not found using the URL Context factory.");
-				}
-			}
-			catch (Exception e) {
-				if (e instanceof NamingException) {
-					// re-throw naming exceptions
-					throw (NamingException) e;
-				}
-
-				NamingException namingException = 
-					new NameNotFoundException("Exception occurred during URL Context Factory Resolution for name = "
-								              + name);
-				namingException.initCause(e);
-				throw namingException;
-			}
-		}
-		else {
-			// treat this lookup as a normal lookup
-			return m_context.lookup(name);
-		}
-
+		return getURLContextOrDefaultContext(name).lookup(name);
 	}
 
-
-	
-
 	public void bind(Name name, Object obj) throws NamingException {
-		m_context.bind(name, obj);
+		getURLContextOrDefaultContext(name.toString()).bind(name, obj);
 	}
 
 	public void bind(String name, Object obj) throws NamingException {
-		m_context.bind(name, obj);
+		getURLContextOrDefaultContext(name).bind(name, obj);
 	}
 
 	public void rebind(Name name, Object obj) throws NamingException {
-		m_context.rebind(name, obj);
+		getURLContextOrDefaultContext(name.toString()).rebind(name, obj);
 	}
 
 	public void rebind(String name, Object obj) throws NamingException {
-		m_context.rebind(name, obj);
+		getURLContextOrDefaultContext(name).rebind(name, obj);
 	}
 
 	public void unbind(Name name) throws NamingException {
-		m_context.unbind(name);
+		getURLContextOrDefaultContext(name.toString()).unbind(name);
 	}
 
 	public void unbind(String name) throws NamingException {
-		m_context.unbind(name);
+		getURLContextOrDefaultContext(name).unbind(name);
 	}
 
 	public void rename(Name oldName, Name newName) throws NamingException {
-		m_context.rename(oldName, newName);
+		getURLContextOrDefaultContext(oldName.toString()).rename(oldName, newName);
 	}
 
 	public void rename(String oldName, String newName) throws NamingException {
-		m_context.rename(oldName, newName);
+		getURLContextOrDefaultContext(oldName).rename(oldName, newName);
 	}
 
 	public NamingEnumeration list(Name name) throws NamingException {
-		return m_context.list(name);
+		return getURLContextOrDefaultContext(name.toString()).list(name);
 	}
 
 	public NamingEnumeration list(String name) throws NamingException {
-		return m_context.list(name);
+		return getURLContextOrDefaultContext(name).list(name);
 	}
 
 	public NamingEnumeration listBindings(Name name) throws NamingException {
-		return m_context.listBindings(name);
+		return getURLContextOrDefaultContext(name.toString()).listBindings(name);
 	}
 
 	public NamingEnumeration listBindings(String name) throws NamingException {
-		return m_context.listBindings(name);
+		return getURLContextOrDefaultContext(name).listBindings(name);
 	}
 
 	public void destroySubcontext(Name name) throws NamingException {
-		m_context.destroySubcontext(name);
+		getURLContextOrDefaultContext(name.toString()).destroySubcontext(name);
 	}
 
 	public void destroySubcontext(String name) throws NamingException {
-		m_context.destroySubcontext(name);
+		getURLContextOrDefaultContext(name).destroySubcontext(name);
 	}
 
 	public Context createSubcontext(Name name) throws NamingException {
-		return m_context.createSubcontext(name);
+		return getURLContextOrDefaultContext(name.toString()).createSubcontext(name);
 	}
 
 	public Context createSubcontext(String name) throws NamingException {
-		return m_context.createSubcontext(name);
+		return getURLContextOrDefaultContext(name).createSubcontext(name);
 	}
 
 	public Object lookupLink(Name name) throws NamingException {
-		return m_context.lookupLink(name);
+		return getURLContextOrDefaultContext(name.toString()).lookupLink(name);
 	}
 
 	public Object lookupLink(String name) throws NamingException {
-		return m_context.lookupLink(name);
+		return getURLContextOrDefaultContext(name).lookupLink(name);
 	}
 
 	public NameParser getNameParser(Name name) throws NamingException {
-		return m_context.getNameParser(name);
+		return getURLContextOrDefaultContext(name.toString()).getNameParser(name);
 	}
 
 	public NameParser getNameParser(String name) throws NamingException {
-		return m_context.getNameParser(name);
+		return getURLContextOrDefaultContext(name).getNameParser(name);
 	}
 
 	public Name composeName(Name name, Name prefix) throws NamingException {
-		return m_context.composeName(name, prefix);
+		return getURLContextOrDefaultContext(name.toString()).composeName(name, prefix);
 	}
 
 	public String composeName(String name, String prefix)
 			throws NamingException {
-		return m_context.composeName(name, prefix);
+		return getURLContextOrDefaultContext(name).composeName(name, prefix);
 	}
 
 	public Object addToEnvironment(String propName, Object propVal)
@@ -224,6 +171,59 @@ class ContextWrapperImpl implements Context {
 		}
 
 		return null;
+	}
+
+	private Context getURLContextOrDefaultContext(String name)
+			throws NameNotFoundException, NamingException {
+		if (isURLRequest(name)) {
+			// attempt to find a URL Context Factory to satisfy this request
+			ObjectFactory objectFactory = null;
+			try {
+				// obtain URL Context Factory in a doPrivilieged() block
+				objectFactory = (ObjectFactory)SecurityUtils.invokePrivilegedAction(new GetObjectFactoryAction(m_factoryManager, name));
+			} catch (Exception e) {
+				logger.log(Level.FINE, 
+						   "Exception occurred while trying to obtain a reference to a URL Context Factory.",
+						   e);
+			}
+			
+			if (objectFactory == null) {
+				throw new NameNotFoundException(
+						"Name: "
+								+ name
+								+ " was not found.  A URL Context Factory was not registered to handle "
+								+ "this URL scheme");
+			}
+
+			try {
+				Context context = 
+					(Context) objectFactory.getObjectInstance(null, null, 
+															  null, m_context.getEnvironment());
+				if (context != null) {
+					return context;
+				}
+				else {
+					throw new NamingException("Name = " + name
+							+ "was not found using the URL Context factory = " + objectFactory);
+				}
+			}
+			catch (Exception e) {
+				if (e instanceof NamingException) {
+					// re-throw naming exceptions
+					throw (NamingException) e;
+				}
+
+				NamingException namingException = 
+					new NameNotFoundException("Exception occurred during URL Context Factory Resolution for name = "
+								              + name);
+				namingException.initCause(e);
+				throw namingException;
+			}
+		}
+		else {
+			// treat this lookup as a normal lookup
+			return m_context;
+		}
 	}
 	
 	private static class GetObjectFactoryAction implements PrivilegedExceptionAction {
